0e3b22e9c834a9e9914c843d7aba9f80c4dc6436
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-04 16:32:54 +0530

    Update ping.py

--- dreamsbot/plugins/ping.py
+++ dreamsbot/plugins/ping.py
@@ -1,1 +1,16 @@
+__PLUGIN__ = "ping"
 
+from time import perf_counter
+from pyrogram import Client
+from pyrogram.types import Message
+from dreamsbot.helpers.decorators import command
+from .modules import register
+
+register("ping", "Bot latency.")
+
+@Client.on_message(command(["ping", "alive"]))
+async def ping(client: Client, message: Message):
+t1 = perf_counter()
+m = await message.reply_text("Pinging...")
+t2 = perf_counter()
+await m.edit_text(f"Pong! <b>{(t2 - t1)*1000:.0f} ms</b>")
