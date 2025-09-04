fc57ee9a5a45db2d896fed1859dd79abf67322d1
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-04 13:56:06 +0530

    Update filters.py

--- dreamsbot/core/filters.py
+++ dreamsbot/core/filters.py
@@ -1,1 +1,6 @@
+from pyrogram import filters
+from config import Config
 
+owner_filter = filters.user(Config.OWNER_ID)
+
+admin_rights = ("can_delete_messages", "can_restrict_members", "can_promote_members")
