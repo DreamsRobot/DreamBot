cd7a44b40a3384d38cc9cfea2b59086afc2ff4ee
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-04 13:58:53 +0530

    Update keyboards.py

--- dreamsbot/helpers/keyboards.py
+++ dreamsbot/helpers/keyboards.py
@@ -1,1 +1,10 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from config import Config
 
+def support_kb():
+btns = []
+if Config.SUPPORT_CHAT:
+btns.append([InlineKeyboardButton("Support", url=f"https://t.me/{Config.SUPPORT_CHAT.strip('@')}")])
+if Config.UPDATE_CHANNEL:
+btns.append([InlineKeyboardButton("Updates", url=f"https://t.me/{Config.UPDATE_CHANNEL.strip('@')}")])
+return InlineKeyboardMarkup(btns) if btns else None
