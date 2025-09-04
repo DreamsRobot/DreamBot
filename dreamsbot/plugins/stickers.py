deac29aa5615cfb2dd3d4c9b68fabf293c3ca7b0
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-04 16:40:39 +0530

    Update stickers.py

--- dreamsbot/plugins/stickers.py
+++ dreamsbot/plugins/stickers.py
@@ -1,1 +1,17 @@
+__PLUGIN__ = "stickers"
 
+from pyrogram import Client
+from pyrogram.types import Message
+from dreamsbot.helpers.decorators import command
+from .modules import register
+
+register("stickers", "Sticker info: reply /sticker to a sticker.")
+
+@Client.on_message(command(["sticker"]))
+async def sticker_info(client: Client, message: Message):
+if not message.reply_to_message or not message.reply_to_message.sticker:
+return await message.reply_text("Reply to a sticker.")
+s = message.reply_to_message.sticker
+await message.reply_text(
+f"Emoji: {s.emoji}\nSet: {s.set_name}\nID: <code>{s.file_unique_id}</code>\nAnimated: {s.is_animated}"
+)
