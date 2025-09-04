a9602e10486fc64ce7bfd74172a3bb345f226480
Bazigar ( CFC ) <hdhindihub@gmail.com>
2025-09-04 13:55:39 +0530

    Update mongo.py

--- dreamsbot/core/mongo.py
+++ dreamsbot/core/mongo.py
@@ -1,1 +1,13 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from config import Config
 
+_client = AsyncIOMotorClient(Config.MONGO_URI)
+db = _client["dreambot"]
+
+# Collections
+col_settings = db["settings"]
+col_warns = db["warns"]
+col_notes = db["notes"]
+col_afk = db["afk"]
+col_blacklist = db["blacklist"]
+col_flood = db["flood"]
